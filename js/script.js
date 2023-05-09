103438a57e7233371f8d0948e5adfd1e02377280
Dev-MuradAbed <93073520+Dev-MuradAbed@users.noreply.github.com>
2023-05-09 17:04:18 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,14 +8,18 @@ document.querySelectorAll('.image-container img').forEach(image => {
 document.querySelector('.pop-image span').onclick = () => {
     document.querySelector('.pop-image').style.display = 'none';
 }
- // Add event listener to the donate PayPal button
- document.querySelector(".donate-paypal").addEventListener("click", function() {
-    window.location.href = "https://www.paypal.com/donate/?hosted_button_id=828SQZ8HAPX3C";
-});
+  // Add event listener to the donate PayPal button
+  document.querySelectorAll(".donate-paypal").forEach(function(button) {
+    button.addEventListener("click", function() {
+      window.location.href = "https://www.paypal.com/donate/?hosted_button_id=828SQZ8HAPX3C";
+    });
+  });
 
-// Add event listener to the donate blockchain button
-document.querySelector(".donate-blockchain").addEventListener("click", function() {
-    // Add your blockchain donation link or code here
-    // For example:
-    window.location.href = "https://commerce.coinbase.com/checkout/35d995c2-cf44-480a-a466-7228bf21eb79";
-});
+  // Add event listener to the donate blockchain button
+  document.querySelectorAll(".donate-blockchain").forEach(function(button) {
+    button.addEventListener("click", function() {
+      // Add your blockchain donation link or code here
+      // For example:
+      window.location.href = "https://commerce.coinbase.com/checkout/35d995c2-cf44-480a-a466-7228bf21eb79";
+    });
+  });
